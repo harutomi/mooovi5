3e95aceca95da0030054366c1a3262f3f2ad5a80
富永治明 <tominagaharuaki@MacBook-Pro.local>
2017-08-24 20:27:11 +0900

    until validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,28 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  # deviseコントローラのアクションが動いた時のみ、configure_permitted_parametersを動かす処理を書く
+  before_action :configure_permitted_parameters, only: :devise
   protect_from_forgery with: :exception
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :avatar])
+  end
+
 end
+
+  class ApplicationController < ActionController::Base
+    # Prevent CSRF attacks by raising an exception.
+    # For APIs, you may want to use :null_session instead.
+    protect_from_forgery with: :exception
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    def after_sign_out_path_for(resource)
+      '/users/sign_in' # サインアウト後のリダイレクト先URL
+    end
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :avatar])
+    end
+
+  end
