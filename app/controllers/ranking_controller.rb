b03ca619d056dffc73251c802a0b0546b2861bb7
富永治明 <tominagaharuaki@MacBook-Pro.local>
2017-08-24 18:32:27 +0900

    small change

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -5,4 +5,5 @@ class RankingController < ApplicationController
     product_ids = Review.group(:product_id).order('count_product_id DESC').limit(5).count(:product_id).keys
     @ranking = product_ids.map{ |id| Product.find(id) }
   end
+
 end
