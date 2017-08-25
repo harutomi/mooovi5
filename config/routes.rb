f2d11848787e70b4738b7cf754852fb0567eb42e
富永治明 <tominagaharuaki@MacBook-Pro.local>
2017-08-25 16:52:09 +0900

    Mission complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 TechReviewSite::Application.routes.draw do
   devise_for :users
+  resources :users, only: :show
   root 'products#index'
   resources :products, only: :show do
     resources :reviews, only: [:new, :create]
